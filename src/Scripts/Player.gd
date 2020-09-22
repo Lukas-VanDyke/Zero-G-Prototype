5be5382572dc9717e1b7dfbd6e4e1dd7b15015e5
Lukas-VanDyke <lukas.vandyke@gmail.com>
2020-09-22 12:38:39 -0600

    Added player state enum and current state variable

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+enum PlayerState{
+	Airborne,
+	Grounded,
+	Sliding,
+	Defeated
+}
+
+var currentState
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
