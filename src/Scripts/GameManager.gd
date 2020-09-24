0e9dac93b929daf1d34ec9ad3f6e648c03776a31
Lukas-VanDyke <lukas.vandyke@gmail.com>
2020-09-24 12:27:21 -0600

    Added skeleton for transitioning between some scenes

--- src/Scripts/GameManager.gd
+++ src/Scripts/GameManager.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+var currentScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func transition_to_combat():
+	pass
+	
+func transition_to_main_menu():
+	pass
