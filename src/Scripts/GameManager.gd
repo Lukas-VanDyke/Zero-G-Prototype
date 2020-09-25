cf3f349d8d180a0b61b83b8578940e054889b7d7
Lukas-VanDyke <lukas.vandyke@gmail.com>
2020-09-25 12:28:27 -0600

    Added some scene transition code to game manager

--- src/Scripts/GameManager.gd
+++ src/Scripts/GameManager.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
-var currentScene
+onready var MainMenuScene = load("res://Scenes/MainMenu.tscn")
+onready var CombatScene = load("res://Scenes/CombatManager.tscn")
+
+var CurrentScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func transition_to_combat():
-	pass
+	clear_scenes()
+	CurrentScene = CombatScene.instance()
+	add_child(CurrentScene)
 	
 func transition_to_main_menu():
-	pass
+	clear_scenes()
+	CurrentScene = MainMenuScene.instance()
+	add_child(CurrentScene)
+	
+#Clear the current scene
+func clear_scenes():
+	if CurrentScene:
+		CurrentScene.queue_free()
+		remove_child(CurrentScene)
+		CurrentScene = null
